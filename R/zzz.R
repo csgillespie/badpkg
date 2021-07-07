dd8c623a485f61633754d7a682956e2ad98d3480
Colin Gillespie <csgillespie@gmail.com>
2021-07-07 23:02:03 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,9 @@ x = r"(
 | {__ \ \_/ /| || `--.   | .--'/  /\  \\     }| |\ \ /  /\  \\  {_ }| {__
 `----' `---' `-'`----'   `-'   `-'  `-' `---' `-' `-'`-'  `-' `---' `----'
 
+By installing this package, I could have done bad things!!
+I didn't though
+I'll talk about this in 5 mins.
 )"
 message(x)
 }
